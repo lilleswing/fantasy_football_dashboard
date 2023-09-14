be1e95823130455d69a7d919175c315d6fe4be5e
Karl Leswing <karl.leswing@gmail.com>
2023-09-13 20:05:12 -0400

    GOGO

--- ffootball/make_dashboard.py
+++ ffootball/make_dashboard.py
@@ -6,9 +6,9 @@ import copy
 from ffootball.get_rosters import get_secrets
 
 
-def bootstrap_median(player_name, window=6, weekly_data=None):
+def bootstrap_median(weekly_data, score_column, player_name, window=6):
     player_df = weekly_data[weekly_data['player_display_name'] == player_name]
-    last_6 = player_df['half_ppr'].values[-1 * window:]
+    last_6 = player_df[score_column].values[-1 * window:]
     if len(last_6) == 0:
         return 0, 0, 0
     bootstraps = np.random.choice(last_6, size=(100, window))
@@ -16,9 +16,9 @@ def bootstrap_median(player_name, window=6, weekly_data=None):
     return np.median(means), np.std(means), len(last_6)
 
 
-def bootstrap_2023(player_name, weekly_data, window=6):
+def bootstrap_2023(weekly_data, score_column, player_name, window=6):
     player_df = weekly_data[weekly_data['player_display_name'] == player_name]
-    last_6 = player_df[player_df['season'] == 2023]['half_ppr']
+    last_6 = player_df[player_df['season'] == 2023][score_column].values
     if len(last_6) == 0:
         return 0, 0, 0
     bootstraps = np.random.choice(last_6, size=(100, window))
@@ -34,24 +34,33 @@ def add_rostered_players(df, fname):
     my_df.to_html(f"scr/{fname}.html")
 
 
-def main():
-    weekly_data = nfl.import_weekly_data(years=[2022, 2023])
-    weekly_data = weekly_data.sort_values('fantasy_points', ascending=False)
-    players = set(weekly_data['player_display_name'])
-    weekly_data = weekly_data.sort_values(['season', 'week'])
-    weekly_data['half_ppr'] = [(x + y) / 2 for x, y in
-                               zip(weekly_data['fantasy_points'], weekly_data['fantasy_points_ppr'])]
-
+def calculate_advs(weekly_data, is_ppr, league_name):
+    """
+    :param weekly_data:
+    :param is_ppr:  str of ("none", 'half', 'full')
+    :param league_name:
+    :return:
+    """
+    if is_ppr == 'half':
+        score_column = 'half_ppr'
+    elif is_ppr == 'none':
+        score_column = 'fantasy_points'
+    elif is_ppr == 'full':
+        score_column = 'fantasy_points_ppr'
+    else:
+        raise ValueError(f"League type of {is_ppr} is not supported")
+    weekly_data = copy.deepcopy(weekly_data)
     position_lookup = {}
     for n, p in zip(weekly_data['player_display_name'], weekly_data['position']):
         position_lookup[n] = p
 
+    players = set(weekly_data['player_display_name'])
     player_adv = {}
     player_adv_2023 = {}
 
     for p in players:
-        player_adv[p] = bootstrap_median(p, window=6, weekly_data=weekly_data)
-        player_adv_2023[p] = bootstrap_2023(p, weekly_data)
+        player_adv[p] = bootstrap_median(weekly_data, score_column, p, window=6)
+        player_adv_2023[p] = bootstrap_2023(weekly_data, score_column, p)
     table = []
     for k in player_adv.keys():
         row = [k, position_lookup[k], *player_adv[k], *player_adv_2023[k]]
@@ -60,10 +69,19 @@ def main():
                                                  'variance_2023', 'games_played_2023'])
     player_adv_df = player_adv_df.sort_values('ADV', ascending=False)
     player_adv_df['rank'] = [x + 1 for x in range(len(player_adv_df))]
+    add_rostered_players(player_adv_df, league_name)
+
+
+def main():
+    weekly_data = nfl.import_weekly_data(years=[2022, 2023])
+    weekly_data = weekly_data.sort_values('fantasy_points', ascending=False)
+    weekly_data = weekly_data.sort_values(['season', 'week'])
+    weekly_data['half_ppr'] = [(x + y) / 2 for x, y in
+                               zip(weekly_data['fantasy_points'], weekly_data['fantasy_points_ppr'])]
 
     secrets = get_secrets()
     for league in secrets['leagues']:
-        add_rostered_players(player_adv_df, league['league_name'])
+        calculate_advs(weekly_data, league['ppr'], league['league_name'])
 
 
 if __name__ == "__main__":
